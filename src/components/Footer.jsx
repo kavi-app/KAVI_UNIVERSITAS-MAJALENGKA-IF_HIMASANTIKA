a952e92707b4d89980f82109d22de73c6f247a58
MnPutrav2 <mochnurdinv2@gmail.com>
2026-09-05 13:25:12 +0700

    footer final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import logo from "../assets/logo/UMC-1.webp";
-import { MapPinned, Phone, Mail } from "lucide-react";
 
 function Footer() {
   return (
@@ -27,23 +26,16 @@ function Footer() {
           </div>
 
           <div className="footer-section">
-            <h3>Hubungi Kami</h3>
-            <p className="footer-location">
-              <MapPinned size={18} />
-              <span>Cirebon, Jawa Barat</span>
-            </p>
-            <p className="footer-phone">
-              <Phone />{" "}
-            </p>
-            <p className="footer-mail">
-              <Mail />{" "}
-            </p>
+            <h3>Media sosial</h3>
+            <a href="https://www.instagram.com/himasantika_umc/">Instagram</a>
+            <a href="https://www.tiktok.com/@himasantika_umc">TikTok</a>
           </div>
 
           <div className="footer-section">
-            <h4>Media Sosial</h4>
-            <a href="https://www.instagram.com/himasantika_umc/">Instagram</a>
-            <a href="https://www.tiktok.com/@himasantika_umc">TikTok</a>
+            <h3>Lokasi</h3>
+            <div className="lg:w-100 lg:h-80 p-2! border border-(--line)">
+              <iframe className="w-full h-full grayscale-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.2249823068105!2d108.49060917585716!3d-6.742388993253898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f1e3b7b1cda1b%3A0xe6245844d4697b1!2sUniversitas%20Muhammadiyah%20Cirebon%20(Pusat)%20(Kampus%202)!5e0!3m2!1sid!2sid!4v1788588584914!5m2!1sid!2sid" loading="lazy"></iframe>
+            </div>
           </div>
         </div>
 
